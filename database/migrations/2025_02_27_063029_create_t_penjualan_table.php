bbb3bef4a7b0dbe30fcfa5994d441703f0fdbbff
Thesambala <zilanzalilan18@gmail.com>
2025-02-27 23:45:50 +0700

    praktikum 2.2

---
+++ database/migrations/2025_02_27_063029_create_t_penjualan_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('pembeli', 50)->unique();
+            $table->string('penjualan_kode', 20);
+            $table->dateTime('penjualan_tanggal');
+            $table->timestamps();
+
+            // Foreign Key ke tabel users (sesuaikan dengan tabel yang benar)
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
