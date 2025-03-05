07b6b2fbc84f2ca1baf5e873ff12618e39481ccb
Thesambala <zilanzalilan18@gmail.com>
2025-03-05 10:55:26 +0700

    Praktikum 2.3 jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,11 @@ class UserController extends Controller
         // ];
         
         //UserModel::create($data);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        
-        return view('user', ['data' => $user]);
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        // return view('user', ['data' => $user]);
+        $userCount = UserModel::where('level_id', 2)->count();
+    
+        return view('user', ['data' => $userCount]);
     }
 }
