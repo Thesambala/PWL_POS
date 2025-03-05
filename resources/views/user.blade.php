27d3ad75c9cf507933529d445ff736f3464ab4cb
Thesambala <zilanzalilan18@gmail.com>
2025-03-05 21:10:37 +0700

    Praktikum 2.4 jobsheet 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,10 +7,16 @@
     <h2><b>Data User</b></h2>
     <table border="1" cellpadding="5" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
