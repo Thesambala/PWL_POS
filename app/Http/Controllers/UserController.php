79953a3d10ca5d6b7cdf82863db0e653680aafee
Thesambala <zilanzalilan18@gmail.com>
2025-03-16 01:30:32 +0700

    Praktikum 1 Jobsheet 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\LevelModel;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -73,8 +74,7 @@ class UserController extends Controller
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) {  // menambahkan kolom aksi 
-                $btn  = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn
-sm">Detail</a> ';
+                $btn  = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn sm">Detail</a> ';
                 $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
                 $btn .= '<form class="d-inline-block" method="POST" action="' .
                     url('/user/' . $user->user_id) . '">'
@@ -149,5 +149,46 @@ sm">Detail</a> ';
         } catch (\Illuminate\Database\QueryException $e) {
             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
+    }
+
+    public function create_ajax()
+    {
+    $level = LevelModel::select('level_id', 'level_nama')->get();
+
+    return view('user.create_ajax')
+        ->with('level', $level);
+    }
+
+    public function store_ajax(Request $request)
+    {
+    // cek apakah request berupa ajax
+    if ($request->ajax() || $request->wantsJson()) {
+        $rules = [
+            'level_id'  => 'required|integer',
+            'username'  => 'required|string|min:3|unique:m_user,username',
+            'nama'      => 'required|string|max:100',
+            'password'  => 'required|min:6'
+        ];
+
+        // use Illuminate\Support\Facades\Validator;
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'   => false, // response status, false: error/gagal, true: berhasil
+                'message'  => 'Validasi Gagal',
+                'msgField' => $validator->errors(), // pesan error validasi
+            ]);
+    }
+
+        UserModel::create($request->all());
+
+        return response()->json([
+            'status'  => true,
+            'message' => 'Data user berhasil disimpan'
+        ]);
+        }
+
+        return redirect('/');
     }
 }
