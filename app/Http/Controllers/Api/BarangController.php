bb4aefa703a27814c680187c7d68a37e85132d45
Thesambala <zilanzalilan18@gmail.com>
2025-05-05 22:30:36 +0700

    Praktikum 1 dan Tugas Jobsheet 11

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -15,8 +16,43 @@ class BarangController extends Controller
 
     public function store(Request $request)
     {
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
+        $validator = Validator::make($request->all(), [
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+            'kategori_id' => 'required|exists:m_kategori,kategori_id',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+
+        ]);
+
+        //if validations fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create user
+        $barang = BarangModel::create([
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'kategori_id' => $request->kategori_id,
+            'image' => $request->image->hashName()
+        ]);
+
+        //return response JSON user is created
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'user' => $barang,
+            ], 201);
+        }
+
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $barang)
--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,14 +15,23 @@ class BarangModel extends Model
     protected $primaryKey = 'barang_id';
 
     protected $fillable = [
+        'barang_kode',
         'barang_nama',
         'kategori_id',
         'harga_beli',
-        'harga_jual'
+        'harga_jual',
+        'image'
     ];
 
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
